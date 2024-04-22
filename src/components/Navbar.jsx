4af7bbe27e617ee4301400da8221feabe46703bb
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-04-22 17:45:34 +0200

    Description concise des changements apportés

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,75 +1,53 @@
 import React from "react";
-import { Link } from 'react-router-dom'; // Importez Link depuis react-router-dom
-import Logo from "../assets/Frame 1.png"
+import { Link } from 'react-router-dom';
+import Logo from "../assets/Frame 1.png";
 
 const Menu = [
-    {
-        title: "Accueil",
-        link: "/"
-    },
-    {
-        title: "Activites",
-        link: "/activites"
-    },
-    {
-        title: "Tarifs",
-        link: "/tarifs"
-    },
-    {
-        title: "A propos",
-        link: "/a-propos"
-    },
+    { title: "Accueil", link: "/" },
+    { title: "Activites", link: "/activites" },
+    { title: "Tarifs", link: "/tarifs" },
+    { title: "A propos", link: "/a-propos" },
 ];
 
 const MenuAdmin = [
-    {
-        title: "Ajout d'articles",
-        link: "/ajout-articles"
-    },
-    
-    {
-        title: "Analyse",
-        link: "/analyse"
-    }
+    { title: "Ajout d'articles", link: "/ajout-articles" },
+    { title: "Analyse", link: "/analyse" }
 ];
 
-const Navbar = (login, logout) => {
-        if (login)
-            return (
-                <div className="mt-12 h-16 flex flex-col items-center justify-center z-60">
-            <img src={Logo} alt="logo" className="h-48 w-64 mt-32" />
-            <div className="flex items-center justify-center mt-10">
-                <ul className="flex flex-row">
+const Navbar = ({ isLoggedIn, logout }) => {
+    if (isLoggedIn) {
+        return (
+            <div className="mt-12 h-16 flex flex-col items-center justify-center z-60">
+                <img src={Logo} alt="Logo" className="h-48 w-64 mt-32" />
+                <ul className="flex items-center justify-center mt-10">
                     {MenuAdmin.map((item) => (
                         <li key={item.title} className="mx-3">
-                            <Link to={item.link} className="text-[#C08D12] font-baloo">{item.title}</Link> {/* Utilisez Link au lieu de a */}
-                            
+                            <Link to={item.link} className="text-[#C08D12] font-baloo">{item.title}</Link>
                         </li>
                     ))}
                     <button 
-                            onClick={logout}
-                            className="p-4 text-white bg-[#C08D12] rounded-full font-baloo "
-                            >Se Déconnecter
-                            </button>
+                        onClick={logout}
+                        className="p-4 text-white bg-[#C08D12] rounded-full font-baloo"
+                    >
+                        Se Déconnecter
+                    </button>
                 </ul>
             </div>
-        </div>
-        )
-        else {
+        );
+    } else {
+        return (
             <div className="mt-12 h-16 flex flex-col items-center justify-center z-60">
-            <img src={Logo} alt="logo" className="h-48 w-64 mt-32" />
-            <div className="flex items-center justify-center mt-10">
-                <ul className="flex flex-row">
+                <img src={Logo} alt="Logo" className="h-48 w-64 mt-32" />
+                <ul className="flex items-center justify-center mt-10">
                     {Menu.map((item) => (
                         <li key={item.title} className="mx-3">
-                            <Link to={item.link} className="text-[#C08D12] font-baloo">{item.title}</Link> {/* Utilisez Link au lieu de a */}
+                            <Link to={item.link} className="text-[#C08D12] font-baloo">{item.title}</Link>
                         </li>
                     ))}
                 </ul>
-                
             </div>
-        </div>
-        }
-}
-export default Navbar;
+        );
+    }
+};
 
+export default Navbar;
