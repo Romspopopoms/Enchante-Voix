035b2b951cbf454b14e279a2fb690008ba0018e9
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-04-22 13:26:17 +0200

    Description concise des changements apportés

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom'; // Importez Link depuis react-router-dom
 import Logo from "../assets/Frame 1.png"
-import { useAuth } from '../AuthContext'; // Assurez-vous que le chemin est correct
 
 const Menu = [
     {
@@ -37,24 +36,8 @@ const MenuAdmin = [
     }
 ];
 
-const Navbar = () => {
-    const { isLoggedIn } = useAuth();
-        if (!isLoggedIn)
-        return  (
-        <div className="mt-12 h-16 flex flex-col items-center justify-center z-60">
-            <img src={Logo} alt="logo" className="h-48 w-64 mt-32" />
-            <div className="flex items-center justify-center mt-10">
-                <ul className="flex flex-row">
-                    {Menu.map((item) => (
-                        <li key={item.title} className="mx-3">
-                            <Link to={item.link} className="text-[#C08D12] font-baloo">{item.title}</Link> {/* Utilisez Link au lieu de a */}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-        )
-        else {
+const Navbar = (login) => {
+        if (login)
             return (
                 <div className="mt-12 h-16 flex flex-col items-center justify-center z-60">
             <img src={Logo} alt="logo" className="h-48 w-64 mt-32" />
@@ -69,6 +52,20 @@ const Navbar = () => {
             </div>
         </div>
         )
-};
+        else {
+            <div className="mt-12 h-16 flex flex-col items-center justify-center z-60">
+            <img src={Logo} alt="logo" className="h-48 w-64 mt-32" />
+            <div className="flex items-center justify-center mt-10">
+                <ul className="flex flex-row">
+                    {Menu.map((item) => (
+                        <li key={item.title} className="mx-3">
+                            <Link to={item.link} className="text-[#C08D12] font-baloo">{item.title}</Link> {/* Utilisez Link au lieu de a */}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+        }
 }
 export default Navbar;
+
