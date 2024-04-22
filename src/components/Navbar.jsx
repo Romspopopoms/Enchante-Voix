e645e9d479b8990470d53c5485a46443ff832cb6
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-04-22 16:41:28 +0200

    Description concise des changements apportés

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,13 +43,14 @@ const Navbar = (login, logout) => {
                     {MenuAdmin.map((item) => (
                         <li key={item.title} className="mx-3">
                             <Link to={item.link} className="text-[#C08D12] font-baloo">{item.title}</Link> {/* Utilisez Link au lieu de a */}
-                            <button 
+                            
+                        </li>
+                    ))}
+                    <button 
                             onClick={logout}
                             className="p-4 text-white bg-[#C08D12] rounded-full font-baloo "
                             >Se Déconnecter
                             </button>
-                        </li>
-                    ))}
                 </ul>
             </div>
         </div>
