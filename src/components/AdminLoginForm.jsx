38be50fbc7f04043b0f734f969066f3758c3f1ac
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-04-23 10:18:28 +0200

    Description concise des changements apportés

--- src/components/AdminLoginForm.jsx
+++ src/components/AdminLoginForm.jsx
@@ -35,14 +35,13 @@ const AdminLoginForm = () => {
         <form onSubmit={handleSubmit}
         className='h-auto w-auto flex flex-col bg-[rgb(252,251,250)] items-center justify-center gap-10 border-2 border-slate-700 rounded-md py-6 px-12 shadow-2xl shadow-black mt-8'>
             <h1 className='font-baloo font-semibold text-4xl text-center mt-4 '>Connexion Admin</h1>
-            <label
-            className="gap-x-4">
+            <label>
                 Nom d'utilisateur:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                <input className="mr-4 border-2 rounded-lg border-black" type="text" value={username} onChange={e => setUsername(e.target.value)} />
             </label>
-            <label className="gap-x-4">
+            <label>
                 Mot de passe:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                <input className="mr-4 border-2 rounded-lg border-black" type="password" value={password} onChange={e => setPassword(e.target.value)} />
             </label>
             <button 
             type="submit"
