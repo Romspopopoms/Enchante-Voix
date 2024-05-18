afe2e54dc499e99587a5eee8c0bc9276ec165c4c
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-19 00:06:46 +0200

    Description concise des changements apportés

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import LogoVox from "../assets/Logo Vox unity V1 1.png"
 const Footer = () => {
     return (
     <div className='bg-[#EFE386] h-auto xl:h-20 flex justify-between'>
-        <div className='w-auto flex flex-col xl:flex-row justify-center items-center mx-8 xl:gap-2'>
+        <div className='w-auto flex flex-col xl:flex-row  justify-center items-center mx-8 xl:gap-2'>
             <h2 className='text-sm xl:text-lg font-baloo text-[#A47A32]'>Developed by</h2>
             <img src={LogoVox} alt="LogoVoxUnity" className='h-auto w-auto' />
             </div>
