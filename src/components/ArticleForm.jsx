bd94edc310e49a932bb263b2c5de9366e82f0c75
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-29 10:20:58 +0200

    Update ArticleForm.jsx

--- src/components/ArticleForm.jsx
+++ src/components/ArticleForm.jsx
@@ -52,6 +52,7 @@ const ArticleForm = () => {
             });
             const file = formData.get('imageFile');
             const blob = await put("test", file, { access: 'public', token: 'vercel_blob_rw_s4TyBQ5DfffM3JDe_Z2HiBFDcrz9YY2dZlZQBhGKjdYXf9o' });
+            alert(blob);
            
             setArticle({ title: '', description: '', videoUrl: '', link: '' });
             setImageFile(null);
