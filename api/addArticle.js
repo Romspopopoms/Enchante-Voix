c798ca43b4b6cf3598e4f96b5e2bd66ba0449616
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-29 16:15:56 +0200

    Update addArticle.js

--- api/addArticle.js
+++ api/addArticle.js
@@ -18,7 +18,6 @@ export default async function handler(req, res) {
     const blob = await put("test", "test SLT", { access: 'public', token: 'vercel_blob_rw_s4TyBQ5DfffM3JDe_Z2HiBFDcrz9YY2dZlZQBhGKjdYXf9o' });
     
     const file = req.body
-    const contentType = req.headers.get('content-type') || 'text/plain'
     const filename = 'TestSLT'
     /*const blob = await put(filename, file, {
         contentType,
