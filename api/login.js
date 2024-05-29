acccf81f2b06ceac305108920d9cdc1cc1f65a5f
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-29 15:30:57 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -12,7 +12,7 @@ const pool = new Pool({
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
-        return res.status(405).json({ message: "login Method not allowed" });
+        return res.status(405).json({ message: "Method not allowed" });
     }
 
     const { username, password } = req.body;
