fa06cee48a0e341893adbdab7243ed2a6b219869
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-29 16:40:32 +0200

    Update ArticleForm.jsx

--- src/components/ArticleForm.jsx
+++ src/components/ArticleForm.jsx
@@ -45,17 +45,12 @@ const ArticleForm = () => {
         
         setLoading(true);
         try {
-            /*const response = await fetch('/api/addArticle', {
+            const response = await fetch('/api/addArticle', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(obj)
-            });*/
-            const response = await fetch('/api/addArticle', {
-                  method: 'POST',
-                  headers: { 'content-type': formData?.type || 'application/octet-stream' },
-                  body: formData
             });
             const data = await response.json();
             
