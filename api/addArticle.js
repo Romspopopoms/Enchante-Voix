9618b4b27657a4a72b66ab2ba974a27b2c2e808b
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-29 16:46:32 +0200

    Update addArticle.js

--- api/addArticle.js
+++ api/addArticle.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
     }
+    return res.status(200).json({ message: 'OK' });
     /*
     const { title, description } = req.body;
 
