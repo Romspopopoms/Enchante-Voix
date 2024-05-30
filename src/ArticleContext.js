cc3dc75dcd6b109c49b0642a780f8c0eedb4667c
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-30 16:59:25 +0200

    Update ArticleContext.js

--- src/ArticleContext.js
+++ src/ArticleContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { put } from '@vercel/blob';
 
 const ArticleContext = createContext();
 
@@ -6,8 +7,11 @@ export const useArticles = () => useContext(ArticleContext);
 
 export const ArticleProvider = ({ children }) => {
     const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const fetchArticles = async () => {
+        setLoading(true);
         try {
             const response = await fetch('/api/getArticles');
             if (response.ok) {
@@ -18,14 +22,14 @@ export const ArticleProvider = ({ children }) => {
             }
         } catch (error) {
             console.error('Error fetching articles:', error);
+            setError(error);
+        } finally {
+            setLoading(false);
         }
     };
 
-    useEffect(() => {
-        fetchArticles();
-    }, []);
-
     const addArticle = async (article) => {
+        setLoading(true);
         try {
             const formData = new FormData();
             formData.append('title', article.title);
@@ -36,10 +40,26 @@ export const ArticleProvider = ({ children }) => {
             formData.append('videoUrl', article.videoUrl);
             formData.append('link', article.link);
 
+            const apiKey = process.env.REACT_APP_BLOB_KEY
+            const file = formData.get('imageFile');
+            const blob = await put(article.title, file, { access: 'public', token: apiKey });
+            const retour = JSON.stringify(blob);
+            const datablob = JSON.parse(retour);
+            const obj = {title: article.title, description: article.description,imageUrl: datablob.url, videoUrl: article.videoUrl, link: article.link};
+            
             const response = await fetch('/api/addArticle', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(obj)
+            });
+            
+            /*const response = await fetch('/api/addArticle', {
                 method: 'POST',
                 body: formData,
             });
+            alert('end test avec fetch');*/
 
             if (response.ok) {
                 const newArticle = await response.json();
@@ -50,11 +70,18 @@ export const ArticleProvider = ({ children }) => {
             }
         } catch (error) {
             console.error('Error adding article:', error);
+            setError(error);
+        } finally {
+            setLoading(false);
         }
     };
 
+    useEffect(() => {
+        fetchArticles();
+    }, []);
+
     return (
-        <ArticleContext.Provider value={{ articles, addArticle, fetchArticles }}>
+        <ArticleContext.Provider value={{ articles, addArticle, fetchArticles, loading, error }}>
             {children}
         </ArticleContext.Provider>
     );
