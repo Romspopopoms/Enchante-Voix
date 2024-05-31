07a4f83e49e7362e4962967e73787c8d8e399d3d
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-31 09:53:16 +0200

    Update ArticlesPage.js

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -33,11 +33,11 @@ const ArticlePage = () => {
             <div key={index} className="bg-white p-4 rounded-lg shadow-lg flex flex-col space-y-3">
               <h3 className='text-xl font-semibold'>{article.title}</h3>
               <p className='text-gray-700'>{article.description}</p>
-              {article.imageUrl && (
-                <img src={article.imageUrl} alt={article.title} className="max-h-40 w-full object-cover rounded-md"/>
+              {article.imageurl && (
+                <img src={article.imageurl} alt={article.title} className="max-h-40 w-full object-cover rounded-md"/>
               )}
-              {article.videoUrl && (
-                <VideoContainer src={article.videoUrl} title={`Embedded YouTube video: ${article.title}`} />
+              {article.videourl && (
+                <VideoContainer src={article.videourl} title={`Embedded YouTube video: ${article.title}`} />
               )}
               {article.link && (
                 <a href={article.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">En savoir plus</a>
