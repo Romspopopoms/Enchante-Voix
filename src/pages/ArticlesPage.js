6d95215be0b56f85dff763835c673b4e5c2d0b7e
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-06-03 12:48:10 +0200

    Description concise des changements apportés

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -11,7 +11,7 @@ const ArticlePage = () => {
   }, [articles.length, fetchArticles]);
 
   const VideoContainer = ({ src, title }) => (
-    <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-lg shadow-lg">
+    <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-lg shadow-lg max-w-[800px]">
       <iframe
         src={src}
         title={title}
@@ -26,14 +26,14 @@ const ArticlePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen overflow-y-auto mt-40 xl:mt-28 gap-y-8">
       <h2 className='text-center text-3xl font-bold text-[#c6941A] mb-5'>Articles</h2>
-      <div className="flex flex-col w-[90%] space-y-6">
+      <div className="flex flex-col w-[90%] space-y-6 items-center">
         {articles.length > 0 ? (
           articles.map((article, index) => (
-            <div key={index} className="bg-white p-6 rounded-lg shadow-lg flex flex-col space-y-4">
+            <div key={index} className="bg-white p-6 rounded-lg shadow-lg flex flex-col space-y-4 max-w-[800px] w-full">
               <h3 className='text-2xl font-semibold text-center'>{article.title}</h3>
               <p className='text-gray-700 text-center'>{article.description}</p>
               {article.imageurl && (
-                <img src={article.imageurl} alt={article.title} className="w-full max-h-[50%] object-cover rounded-md"/>
+                <img src={article.imageurl} alt={article.title} className="w-full max-h-[450px] object-cover rounded-md"/>
               )}
               {article.videourl && (
                 <VideoContainer src={article.videourl} title={`Embedded YouTube video: ${article.title}`} />
