2176d84689384d4b178d9f307421ca579ba88ee3
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-06-03 12:43:46 +0200

    Description concise des changements apportés

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react';
 import { useArticles } from '../ArticleContext';
 
 const ArticlePage = () => {
-  const { articles, fetchArticles } = useArticles(); // Utilisation de fetchArticles défini dans ArticleContext
+  const { articles, fetchArticles } = useArticles();
 
   useEffect(() => {
     if (articles.length === 0) {
       fetchArticles();
     }
-  }, [articles.length, fetchArticles]);  // Dépendance ajoutée pour suivre les changements correctement
-
+  }, [articles.length, fetchArticles]);
 
   const VideoContainer = ({ src, title }) => (
     <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-lg shadow-lg">
@@ -25,27 +24,27 @@ const ArticlePage = () => {
   );
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen overflow-y-auto mt-40 xl:mt-28 gap-y-8">
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-y-auto mt-40 xl:mt-28 gap-y-8">
       <h2 className='text-center text-3xl font-bold text-[#c6941A] mb-5'>Articles</h2>
-      <div className="flex flex-col w-[90%]">
+      <div className="flex flex-col w-[90%] space-y-6">
         {articles.length > 0 ? (
           articles.map((article, index) => (
-            <div key={index} className="bg-white p-4 rounded-lg shadow-lg flex flex-col space-y-3">
-              <h3 className='text-xl font-semibold text-center'>{article.title}</h3>
-              <pre className='text-gray-700 text-center'>{article.description}</pre>
+            <div key={index} className="bg-white p-6 rounded-lg shadow-lg flex flex-col space-y-4">
+              <h3 className='text-2xl font-semibold text-center'>{article.title}</h3>
+              <p className='text-gray-700 text-center'>{article.description}</p>
               {article.imageurl && (
-                <img src={article.imageurl} alt={article.title} className="max-h-[50%] w-full object-cover rounded-md"/>
+                <img src={article.imageurl} alt={article.title} className="w-full max-h-[50%] object-cover rounded-md"/>
               )}
               {article.videourl && (
                 <VideoContainer src={article.videourl} title={`Embedded YouTube video: ${article.title}`} />
               )}
               {article.link && (
-                <a href={article.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">En savoir plus</a>
+                <a href={article.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline text-center">En savoir plus</a>
               )}
             </div>
           ))
         ) : (
-          <p className="text-white w-full text-center">Aucun article trouvé.</p>
+          <p className="text-gray-700 w-full text-center">Aucun article trouvé.</p>
         )}
       </div>
     </div>
