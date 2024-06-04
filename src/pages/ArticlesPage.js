37107d70b20b6f104bca91d2a39299ce96111360
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-06-04 10:07:54 +0200

    Update ArticlesPage.js

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -34,6 +34,26 @@ const ArticlePage = () => {
     </div>
   );
 
+  const SectionContainer = ({ title }) => (
+   <div id={title} className="flex flex-col w-[90%] space-y-6 items-center">
+      {articles.filter(article => article.category === {title}).map((article, index) => (
+        <div key={index} className="bg-white p-6 rounded-lg shadow-lg flex flex-col space-y-4 max-w-[800px] w-full">
+          <h3 className='text-2xl font-semibold text-center'>{article.title}</h3>
+          <p className='text-gray-700 text-center'>{article.description}</p>
+          {article.imageurl && (
+            <img src={article.imageurl} alt={article.title} className="w-full max-h-[450px] object-cover rounded-md"/>
+          )}
+          {article.videourl && (
+            <VideoContainer src={article.videourl} title={`Embedded YouTube video: ${article.title}`} />
+          )}
+          {article.link && (
+            <a href={article.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline text-center">En savoir plus</a>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen overflow-y-auto mt-40 xl:mt-28 gap-y-8">
       <h2 className='text-center text-3xl font-bold text-[#c6941A] mb-5'>Articles</h2>
